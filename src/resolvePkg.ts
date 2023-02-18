3047d050e2ab439830eec12bb8e72023962ff78a
Jufeng Zhang <zjffun@gmail.com>
2023-02-18 13:24:40 +0800

    fix: require is not defined in ES module scope

--- src/resolvePkg.ts
+++ src/resolvePkg.ts
@@ -1,10 +1,20 @@
 import { fileURLToPath } from "node:url";
 
 export default async function (name) {
-  if (typeof require?.resolve === "function") {
+  let usingRequire = true;
+  try {
+    usingRequire = typeof require === "function";
+  } catch {
+    // ReferenceError: require is not defined in ES module scope, you can use import instead
+    usingRequire = false;
+  }
+
+  // CJS
+  if (usingRequire) {
     return require.resolve(name);
   }
 
+  // ESM
   // @ts-ignore
   let url = await import.meta.resolve(name);
 
